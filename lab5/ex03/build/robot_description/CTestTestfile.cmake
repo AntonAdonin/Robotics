746e0d8f7bad0a204de0ba43181d4f6973d4a7a3
Антон Адонин <adonin-2004@mail.ru>
2024-11-20 10:33:31 +0700

    Delete lab5/ex03/build directory

--- lab5/ex03/build/robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antonadonin/Robotics/lab5/ex03/robot_description
-# Build directory: /home/antonadonin/Robotics/lab5/ex03/build/robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
